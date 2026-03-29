15d7dd26ca925fd410772a6ee113f42ea7e38da3
Luisa <luisamatosisa@gmail.com>
2026-03-29 16:40:07 -0300

    Atualizando portfólio

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ if (carousel && prevBtn && nextBtn) {
     carousel.scrollBy({ left: -320, behavior: 'smooth' });
   });
 }
+
+const elements = document.querySelectorAll(
+  ".voluntariado-text, .voluntariado-images img"
+);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+elements.forEach(el => observer.observe(el));
+
